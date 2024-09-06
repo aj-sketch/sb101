470dce8971347d0503218f76f1430e8321dc7010
Ajayram Pandian <ajayram.p@kgisl.com>
2024-09-06 17:17:14 +0530

    First commit

---
+++ src/main/resources/schema.sql
@@ -0,0 +1,5 @@
+create table if not exists person( 
+    id BIGINT NOT NULL PRIMARY KEY,
+    uname VARCHAR(255) NOT NULL,
+    email VARCHAR(255) NOT NULL
+);
---
+++ src/main/resources/static/index.js
@@ -0,0 +1,7 @@
+// const uri = "http://localhost:8080/person";
+// function getItems() {
+//     fetch(uri)
+//       .then((response) => response.json())
+//       .then((data) => console.log(data))
+//       .catch((error) => console.error("Unable to get items.", error));
+//   }
